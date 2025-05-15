2cc1a8b989128e452326266a5a0de5f9ffefc51b
Leonardo Berton <leozberton@gmail.com>
2025-05-15 11:21:47 -0300

    Update

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import psycopg2
+import numpy as np
 from datetime import datetime
 from streamlit_autorefresh import st_autorefresh
 from sqlalchemy import create_engine
